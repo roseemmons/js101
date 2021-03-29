45ce1bc3d98624f009d99f33385dd1c51a116fd6
Rose M Emmons <roseemmons@Roses-15-in-MBP.local>
2021-03-29 00:14:17 -0600

    Finished v1.0

--- lesson_6/21.js
+++ lesson_6/21.js
@@ -4,10 +4,10 @@
 ** 3. Player turn: hit or stay  - DONE
 **    - repeat until bust or stay
 ** 4. If player bust, dealer wins. - DONE
-** 5. Dealer turn: hit or stay - IN PROG
+** 5. Dealer turn: hit or stay - DONE
 **    - repeat until total >= 17
-** 6. If dealer busts, player wins.
-** 7. Compare cards and declare winner.
+** 6. If dealer busts, player wins. - DONE
+** 7. Compare cards and declare winner. - DONE
 */
 
 const readline = require("readline-sync");
@@ -24,14 +24,6 @@ function newDeck(suitArr, cardsArr) {
     result[element] = cardsArr.slice();
   });
 
-  return result;
-}
-
-
-function formattedCard(card) {
-  let result = ``;
-  let [suit, cardNumber] = card;
-  result += `${cardNumber} of ${suit}`;
   return result;
 }
 
@@ -106,7 +98,7 @@ function hit(hand, deck) {
 }
 
 
-function checkForBlackJack(players) {
+function checkFor21(players) {
   /*
   ** Data Structure
   **  [
@@ -118,13 +110,13 @@ function checkForBlackJack(players) {
   **    { currentHand:  [ ["Diamonds", "Ace"], ["Hearts", 10] ], handValue: 21, busted: false, gamesWon: 0 }
   **  ]
   */
-  let winners = Object.entries(players).filter(player => player[1]['handValue'] === LIMIT);
+  return Object.entries(players).filter( player => player[1]['handValue'] === LIMIT );
+}
 
-  if (winners.length > 0) {
-    winners.forEach(player => player[1]['gamesWon'] += 1);
-  }
-
-  return winners; 
+function determineWinner(playersWhoHaveNotBusted) {
+    let maxCallback = ( max, cur ) => Math.max( max, cur );
+    let highestScore = playersWhoHaveNotBusted.map( player => player[1]['handValue'] ).reduce( maxCallback, -Infinity ); // returns a Number
+    return playersWhoHaveNotBusted.filter( player => player[1]['handValue'] === highestScore );
 }
 
 
@@ -141,7 +133,6 @@ function initGame(playerNames) {
     game['players'][playerName] = {
       'currentHand': 0,
       'handValue': 0,
-      'busted': false,
       'gamesWon': 0
     };
   }); 
@@ -151,8 +142,9 @@ function initGame(playerNames) {
 
 
 // LOGIC
-// 1. Create the game object
-let game = initGame( ['Dealer', 'Player1'] );
+// 1. Create the game object and welcome messages
+let game = initGame( ['Dealer', 'Player1', 'Player2'] );
+console.log(`\n*** Welcome to Command Line Black Jack! ***`);
 
 
 while ( game['activeGame'] ) {
@@ -161,34 +153,41 @@ while ( game['activeGame'] ) {
   game['gamesPlayed'] += 1;
   
 
-  // 3. Create arrays for players and winners to use later
+  // 3. Create an array for the player names
   let playerNames = Object.keys( game['players'] ); // Example: ['dealer', 'player1']
 
 
-  // 4. Create welcome messages
-  console.log(`*** Welcome to Command Line Black Jack! ***`);
-  console.log(`Good luck to all our players: ${playerNames}\n`);
-  console.log(`Round ${game['gamesPlayed']}`);
+  // 4. Announce the current round
+  console.log(`\n\n\n=== Round ${game['gamesPlayed']} ===`);
+  console.log("Good luck to all our players:");
+  playerNames.forEach( player => console.log(`- ${player}`) );
+
 
 
   // 5. Deal cards to all the players and tally up the hands
+  //   TODO: Explore passing in the player object instead.
+  console.log("\n\n** The dealer is dealing out cards **");
+
   playerNames.forEach(function(playerName) {
     game['players'][playerName]['currentHand'] = dealCards(game['deck']);
     game['players'][playerName]['handValue'] = tallyHand( game['players'][playerName]['currentHand'] );
-    console.log(`${playerName}'s hand contained ${game['players'][playerName]['currentHand']} for a value of ${game['players'][playerName]['handValue']}.`);
+    game['players'][playerName]['busted'] = false;
+    console.log(`${playerName}'s hand contains ${game['players'][playerName]['currentHand']} for a value of ${game['players'][playerName]['handValue']}.`);
   });
+
 
   // 6. Does anyone have Black Jack?
   //    If yes, end the game
   //    If no, ask each player to hit or stay
-  let winners = checkForBlackJack( game['players'] );
+  let playersWith21 = checkFor21( game['players'] );
 
-  if (winners.length > 0) {
-    let winnerNames = winners.map( player => player[0] );
+  if ( playersWith21.length > 0 ) {
+    console.log("\n\nCongrats on Black Jack to these players:");
 
-    console.log("Congrats on Black Jack to these players:");
-    winnerNames.forEach( winnerName => console.log(winnerName) );
-
+    playersWith21.forEach( player => {
+      console.log(`- ${player[0]}`);
+      game['players'][player[0]]['gamesWon'] += 1;
+    });
   } else {
     playerNames.forEach(function(playerName) {
 
@@ -197,36 +196,98 @@ while ( game['activeGame'] ) {
         let hitOrStayAnswer = null;
 
         do {
-          console.log(`\n${playerName}, would you like to hit or stay?`);
-          console.log("Type Hit or Stay and press Enter.");
+          console.log(`\n\n=== ${playerName}'s Turn ===`);
+          console.log(`Hand: ${playerData['currentHand']}`);
+          console.log(`Value: ${playerData['handValue']}`);
+          console.log("Would you like to hit or stay?\n(Type Hit or Stay and press Enter.)");
           hitOrStayAnswer = readline.question().toLowerCase();
 
           switch (hitOrStayAnswer) {
-            case "hit":  // Add card to the hand and tally the hand's value
+            case "hit":
+              console.log("\n\n** The dealer has given you a card. **");
               hit( playerData['currentHand'], game['deck'] );
               playerData['handValue'] = tallyHand( playerData['currentHand'] );
-              console.log(`\nYour hand contains: ${playerData['currentHand']} for a value of ${playerData['handValue']}.`);
               break;
+
             case "stay":
-              console.log("\nYou chose to stay.");
+              console.log(`\n\n** You chose to stay at ${playerData['handValue']} **`);
               break;
+
             default:
-              console.log("\nThat is not a valid answer.");
+              console.log("\n\nThat is not a valid answer.");
               break;
           }
         } while ( hitOrStayAnswer !== "stay" && playerData['handValue'] < LIMIT );
 
+        // 6a. Check if the player busted
         if ( playerData['handValue'] > LIMIT ) {
-          console.log("\nYou busted.");
+          playerData['busted'] = true;
+
+          console.log(`\n\n** You busted **`);
+          console.log(`Hand: ${playerData['currentHand']}`);
+          console.log(`Value: ${playerData['handValue']}`);
         } else {
-          console.log(`\nYour hand contains: ${playerData['currentHand']} for a value of ${playerData['handValue']}.`);
+          console.log("Next player's turn...");
         }
-      } // End if (playerName !== "dealer") branch
-    }); // End forEach loop
-  } // End else branch
+      }
+    });
 
 
-  // TODO: Compare dealer and player hands
+    // 7. If all players have busted, the Dealer wins.
+    //    Otherwise, the Dealer must auto hit if handValue is less than 17
+    //    Dealer will stay if the hand value is 17 or more
+    let didAllPlayersBust = Object.entries(game['players']).filter(player => player[0] !== "Dealer").every(player => player[1]['busted'] === true);
+    
+    if ( didAllPlayersBust ) {
+      console.log("\n\nAll players have busted. Dealer wins!");
+      game['players']['Dealer']['gamesWon'] += 1;
+    } else {
+      let dealerData = game['players']['Dealer'];
+
+      while ( dealerData['handValue'] < 17 ) {
+        console.log(`\n\n=== Dealers's Turn ===`);
+        console.log(`Hand: ${dealerData['currentHand']}`);
+        console.log(`Value: ${dealerData['handValue']}`);
+        console.log("Dealer has less than 17 and must hit.");
+        console.log("\n\n** The dealer has taken a card. **");
+
+        hit( dealerData['currentHand'], game['deck'] );
+        dealerData['handValue'] = tallyHand( dealerData['currentHand'] );
+      }
+
+
+      if ( dealerData['handValue'] > LIMIT ) {
+        dealerData['busted'] = true;
+        console.log("\n\n** Dealer busted **");
+      } else {
+        console.log(`\n\n** Dealer will stay **`);
+      }
+
+      console.log(`Hand: ${dealerData['currentHand']}`);
+      console.log(`Value: ${dealerData['handValue']}`);
+
+
+      // 8. Now we check for a winner
+      let playersWhoHaveNotBusted = Object.entries(game['players']).filter(player => player[1]['busted'] === false);
+
+      if ( playersWhoHaveNotBusted.length === 1 ) {
+        console.log(`\n\nCongratulations to ${playersWhoHaveNotBusted[0][0]} for winning this round!`);  
+        game['players'][playersWhoHaveNotBusted[0][0]]['gamesWon'] += 1;
+      } else {
+        let winners = determineWinner(playersWhoHaveNotBusted);
+
+        console.log("\n\nCongratulations to our winners:");
+        winners.map( winner => {
+          console.log(`- ${winner[0]}`);
+          game['players'][winner[0]]['gamesWon'] += 1;
+        });
+      }
+    }    
+  }
+
+
+  console.log("\n\n*** Scoreboard ***");
+  Object.entries(game['players']).forEach(player => console.log(`${player[0]}: ${player[1]['gamesWon']}`) );
 
   console.log("\nWould you like to play another round?");
   console.log("Type Yes or No and press Enter.");
